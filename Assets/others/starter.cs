4e77390d4efa4eb80c99a59d5376cde1834f6dc7
Lambe Aryaan Ashraf Ali <aryaanlambe@outlook.com>
2021-04-17 23:11:51 +0200

    Migrated local files to GitHub repo.

---
+++ Assets/others/starter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class starter : MonoBehaviour
+{
+    public string nextscene = "";
+    float countDown = 1f;
+    bool triggered = false;
+
+    void FixedUpdate()
+    {
+        if (countDown > 0)
+            countDown -= Time.deltaTime;
+        else {
+            if (!triggered)
+            {
+                triggered = true;
+                SceneManager.LoadSceneAsync(nextscene);
+
+            }
+        }
+
+    }
+}
